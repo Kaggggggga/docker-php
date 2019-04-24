f1ab5f0ce3174c0de413b1a05c2e5f15424308d1
Kaggggggga <kaga.fromearth@gmail.com>
2019-04-24 21:34:54 +0800

    feat: add creating log fifo file when laravel init

--- base/scripts/init.d/init-laravel.sh
+++ base/scripts/init.d/init-laravel.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
+if [[ ! -z "$LARAVEL_LOG_PATH" ]]; then
+    echo "init laravel log pipe '$LARAVEL_LOG_PATH'"
+    mkfifo $LARAVEL_LOG_PATH
+    chmod 777 $LARAVEL_LOG_PATH
+fi
 
 artisan=$LARAVEL_ROOT/artisan
 if [ -f $artisan ]; then
-    if [ "$LARAVEL_CONFIG_CACHE" -eq "1" ]; then
+    if [[ ! -z "$LARAVEL_CONFIG_CACHE" ]]; then
         echo "laravel config:cache"
         php $artisan config:cache
     fi
-    if [ "$LARAVEL_ROUTE_CACHE" -eq "1" ]; then
+    if [[ ! -z "$LARAVEL_ROUTE_CACHE" ]]; then
         echo "laravel route:cache"
         php $artisan route:cache
     fi
+
 fi
